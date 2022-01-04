20d707572fc9141398863ee7f28a1c8cc21051d8
ms <mhw828@sina.com>
2022-01-04 10:46:54 +0800

    更新代码

--- src/main/java/org/ms/utils/gson/GsonUtils.java
+++ src/main/java/org/ms/utils/gson/GsonUtils.java
@@ -22,20 +22,20 @@ import com.google.gson.JsonSyntaxException;
 
 public final class GsonUtils {
 
-    private GsonUtils() {}
+    private GsonUtils() {
+    }
 
     private static final Gson gson = new Gson();
 
-    public static final Gson getGson() {
+    public static Gson getGson() {
         return gson;
     }
 
-    public static final String toJson(Object obj) {
+    public static String toJson(Object obj) {
         return gson.toJson(obj);
     }
 
-    public static final <T> T fromJson(String json, Class<T> classOfT) throws JsonSyntaxException {
+    public static <T> T fromJson(String json, Class<T> classOfT) throws JsonSyntaxException {
         return gson.fromJson(json, classOfT);
     }
-
 }
